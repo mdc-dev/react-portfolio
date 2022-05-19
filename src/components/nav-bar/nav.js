eee406d8d7448f3d9b4dd1f9a51738d5f436e1e3
Matthew Courtney <mcourtdev@gmail.com>
2022-05-19 01:07:58 -0400

    got a responsive nav bar need to get sidebar up and running tomorrow

---
+++ src/components/nav-bar/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './nav.scss';
+
+function Navbar() {
+    return (
+    <nav className='nav-bar'>
+        <div className='logo'>Logo</div>
+        <div className='hamburger'>|||</div>
+        <ul className="nav-items">
+            <li>About Me</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+    </nav>   
+    )
+}
+
+
+export default Navbar;
